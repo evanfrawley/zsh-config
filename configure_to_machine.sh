b59b187f2f6d94d9b5030c17896720d794ab4439
evanfrawley <frawley@uw.edu>
2018-03-20 15:52:45 -0700

    augment configuration script

--- configure_to_machine.sh
+++ configure_to_machine.sh
@@ -1,24 +1,43 @@
 #!/usr/bin/env bash
 
-echo "*******************"
-echo "** Downloading zsh"
-echo "*******************"
+print_cmd() {
+  echo "*******************"
+  echo "** ${1}"
+  echo "*******************"
+}
+
+install_zsh() {
+  print_cmd("Installing zsh")
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
+
+install_thefuck_linux() {
+  print_cmd("Installing thefuck")
+  sudo apt update
+  sudo apt install python3-dev python3-pip
+  sudo pip3 install thefuck
+}
+
+install_thefuck_mac() {
+  print_cmd("Installing thefuck")
+  brew install thefuck
+}
+
+print_cmd("Downloading zsh")
 unameOut="$(uname -s)"
 case "${unameOut}" in
-    Linux*)     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";;
-    Darwin*)    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";;
+    Linux*)     install_zsh(); install_thefuck_linux();;
+    Darwin*)    install_zsh(); install_thefuck_mac();;
     *)          machine="UNKNOWN:${unameOut}, please use a real dev env"
 esac
 echo ${machine}
 
 set -e
-echo "*******************"
-echo "** Setting up..."
-echo "*******************"
+
+print_cmd("Setting up config files")
 
 cp ~/zsh-config/.zshrc ~/.zshrc
+cp ~/zsh-config/secrets.sh ~/secrets.sh
 source ~/.zshrc
 
-echo "*******************"
-echo "** Done."
-echo "*******************"
+print_cmd("Done")
