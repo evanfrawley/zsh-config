11c2faef6ffba0d1e458169e418f046b480dbcc3
evanfrawley <frawley@uw.edu>
2018-03-20 16:12:39 -0700

    finish updating update script

--- update_from_repo.sh
+++ update_from_repo.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env zsh
 
+print_cmd() {
+  echo "*******************"
+  echo "** $1"
+  echo "*******************"
+}
+
 cd ~/zsh-config
+
+print_cmd "Pulling repo"
+
 git pull
+
+print_cmd "Copying config"
+
 cp ~/zsh-config/.zshrc ~/.zshrc
 cp ~/zsh-config/secrets.zsh ~/secrets.zsh
+
+print_cmd "Sourcing config"
+
 source ~/.zshrc
