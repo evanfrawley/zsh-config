4ae22e3c5ca31c17740b5da5151e8d9b42721aa1
evanfrawley <frawley@uw.edu>
2018-03-20 15:55:16 -0700

    fixing func call

--- configure_to_machine.sh
+++ configure_to_machine.sh
@@ -7,23 +7,23 @@ print_cmd() {
 }
 
 install_zsh() {
-  print_cmd("Installing zsh")
+  print_cmd "Installing zsh"
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
 
 install_thefuck_linux() {
-  print_cmd("Installing thefuck")
+  print_cmd "Installing thefuck"
   sudo apt update
   sudo apt install python3-dev python3-pip
   sudo pip3 install thefuck
 }
 
 install_thefuck_mac() {
-  print_cmd("Installing thefuck")
+  print_cmd "Installing thefuck"
   brew install thefuck
 }
 
-print_cmd("Downloading zsh")
+print_cmd "Downloading zsh"
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)     install_zsh(); install_thefuck_linux();;
@@ -34,10 +34,10 @@ echo ${machine}
 
 set -e
 
-print_cmd("Setting up config files")
+print_cmd "Setting up config files"
 
 cp ~/zsh-config/.zshrc ~/.zshrc
 cp ~/zsh-config/secrets.sh ~/secrets.sh
 source ~/.zshrc
 
-print_cmd("Done")
+print_cmd "Done"
